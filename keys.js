54f02c88e0987c6315dcf72c42bc1c99b1c16280
Esther <estshwang@gmail.com>
2019-05-19 08:47:23 -0500

    added base files nad starting to put in API keys

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bandsInTown = {
+    key: process.env.BANDS_IN_TOWN_KEY
+};
+
+exports.Omdb = {
+    key: process.env.OMDB_API_KEY
+};
---
+++ liri.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new spotify(keys.spotify);
+
+//take commands from command line:
+var cmdSearch = process.argv[2];
+
+concert-this
+
+//search Bands in Town Artist Events API
+var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=
+
+
+spotify-this-song
+
+movie-this
+
+do-what-it-says
