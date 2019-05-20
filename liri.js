0d946bcd959588250095fdc9faab8295947a478a
Esther <estshwang@gmail.com>
2019-05-20 13:45:11 -0500

    able to run axios function successfully with bands in town. working on parsing response

--- liri.js
+++ liri.js
@@ -2,15 +2,15 @@
 //instantiation of variables
 require("dotenv").config();
 var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
-var spotify = new spotify(keys.spotify);
+// var Spotify = require("node-spotify-api");
+// var spotify = new spotify(keys.spotify);
 // var bInTwn = new bands(keys.bandsInTown);
 // var omdb = new omdb(keys.omdb);
 let axios = require("axios");
 
 //variables from command line:
-var cmd = process.argv[2];
-var cmdSearch = process.argv[3];
+let cmd = process.argv[2];
+let cmdSearch = process.argv[3];
 
 // concert-this
 
@@ -24,33 +24,36 @@ if(cmd === "concert-this") {
             //want to check if status is ok within axio call
             if (response.status === 200) {
                 console.log(response.data);
-                // console.log("the movie's release year is: " +response.data.Year);
+                //name of venue
+                //venue location
+                //date of event (MM/DD/YYYY) (moment)
             }
             
         })
         .catch(function(error){
     
         });
+    }        
     
-} else if( cmd === "spotify-this-song") {
-    Spotify.search({type: 'track', query: cmdSearch })
-    .then(function(response) {
-        console.log(response);
-        //console.log:
-        //artist
-        //song name
-        //preview link of song from spotify
-        //album song is from
-        //else default song
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-} else if( cmdSearch === undefined) {
-    //search default song
-} else if (cmd === "movie-this") {
-    let movieUrl = 
-}
+// } else if( cmd === "spotify-this-song") {
+//     Spotify.search({type: 'track', query: cmdSearch })
+//     .then(function(response) {
+//         console.log(response);
+//         //console.log:
+//         //artist
+//         //song name
+//         //preview link of song from spotify
+//         //album song is from
+//         //else default song
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     })
+// } else if( cmdSearch === undefined) {
+//     //search default song
+// } else if (cmd === "movie-this") {
+//     let movieUrl = 
+// }
 
 
 
