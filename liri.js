0ee11d3e023da999e4c509bb6613728c27ecd2f9
Esther <estshwang@gmail.com>
2019-05-20 22:15:33 -0500

    bands in town response parsed and formatted.Working successfully

--- liri.js
+++ liri.js
@@ -26,44 +26,49 @@ if(cmd === "concert-this") {
     let queryUrl = "https://rest.bandsintown.com/artists/" + cmdSearch + "/events?app_id=codingbootcamp";
     
     axios.get(queryUrl).then(
-        function(response) {
-            // let resParse = JSON.parse(reponse);
-            // if (resParse.length > 0) {
-                for(let i = 0; i < 1 ; i++) {
-                
-                    //name of venue
-                    console.log(response.data);
-                    //venue location
-                    // console.log("The venue location is: " + response.data.venue.city + ", " + response.data.venue.region);
-                    //date of event (MM/DD/YYYY) (moment)
-                    // console.log("The show date is: " + moment(response.data.datetime).format(MM/DD/YYYY));
-                }
-            // }
+        function(response){
+        //   console.log(response.data);
+        //capture data into variable
+        var rData = response.data;
+        
+        //in order to access data, loop through object array
+        for (let i = 0; i < rData.length; i++ ){
+
+            //log venue name and location
+            console.log(
+                "========" + rData[i].venue.city + "," + rData[i].venue.region + "===========" + 
+                "\nVenue Name:" + rData[i].venue.name + 
+                "\nVenue Location: " + rData[i].venue.city + "," + rData[i].venue.region);
+            
+                //log concert date
+                var conDate = moment(rData[i].datetime).format("MM/DD/YYYY");
+            console.log("Date of concert: " + conDate + "\n=================================\n\n");
+            }
+
         })
         .catch(function(err){
             console.log(err);
         });
-            
+    } else if(cmdSearch === undefined){
+        console.log("Please enter Band name or Artist name.")
+    }
+// } else if( cmd === "spotify-this-song") {
+//     spotify.search({type: 'track', query: cmdSearch, limit: 1 })
+//         .then(function(response) {
+//             console.log(response);
+//             //console.log:
+//             //artist
+//             //song name
+//             //preview link of song from spotify
+//             //album song is from
+//             //else default song
+//         })
+//         .catch(function(err){
+//             console.log(err);
+//         })
+//     })
     
-} else if (cmd === undefined) {
-    //search default band
-} else if( cmd === "spotify-this-song") {
-    spotify.search({type: 'track', query: cmdSearch, limit: 1 }, function(error, data){
-        .then(function(response) {
-            console.log(response);
-            //console.log:
-            //artist
-            //song name
-            //preview link of song from spotify
-            //album song is from
-            //else default song
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-    })
-    
-}
+// }
 
 // } else if( cmdSearch === undefined) {
 //     //search default song from random text file
@@ -85,7 +90,7 @@ if(cmd === "concert-this") {
 //         );
 // } else if (cmd === undefined) {
     //console.log Mr. Nobody 
-} else if (cmd === "do-what-it-says") {
+// } else if (cmd === "do-what-it-says") {
 //     fs.readFile
 // }
 
